1e739e425e2350bd7a427f69b66df997d674f475
Jacob <jacobm3@gmail.com>
2025-10-20 09:07:21 -0500

    Update setup-lists.sh

--- pihole/setup-lists.sh
+++ pihole/setup-lists.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Wait until Pi-hole container is healthy
+until [ "$(docker inspect -f '{{.State.Health.Status}}' pihole 2>/dev/null)" = "healthy" ]; do
+  echo "⏳ Waiting for pihole to become healthy..."
+  sleep 3
+done
+echo "✅ pihole is healthy!"
+
+
+
+docker exec pihole pihole-FTL --config database.queryLogging false
+docker exec pihole pihole-FTL --config database.maxDBdays 1
+docker exec pihole pihole-FTL --config database.IPaddrDB true
+
+
 sqlite3 etc-pihole/gravity.db "DELETE FROM adlist;"
 sqlite3 etc-pihole/gravity.db "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts', 'Steven Black unified');"
 sqlite3 etc-pihole/gravity.db "INSERT OR IGNORE INTO adlist (address, comment) VALUES ('https://phishing.army/download/phishing_army_blocklist_extended.txt', 'Phishing Army extended');"
