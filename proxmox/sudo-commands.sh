9142ff646067fb70d3ae093304a2e15acb97eacf
Jacob <jacobm3@gmail.com>
2026-06-13 19:21:41 -0500

    update

---
+++ proxmox/sudo-commands.sh
@@ -0,0 +1,24 @@
+# Proxmox VM / CT management
+alias qm='sudo qm'
+alias pct='sudo pct'
+
+# Proxmox storage / API / node management
+alias pvesm='sudo pvesm'
+alias pvesh='sudo pvesh'
+alias pvenode='sudo pvenode'
+
+# Backups / restores
+alias vzdump='sudo vzdump'
+alias qmrestore='sudo qmrestore'
+
+# User / permission management
+alias pveum='sudo pveum'
+
+# Cluster management, useful even on a single-node box for status/info
+alias pvecm='sudo pvecm'
+
+# Firewall
+alias pve-firewall='sudo pve-firewall'
+
+# HA, only useful if you use HA/clustering
+alias ha-manager='sudo ha-manager'
